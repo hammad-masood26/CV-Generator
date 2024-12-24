3d9983863349992a5fd75fa307c791c5b50cbdcf
Hammad Masood <newemail@example.com>
2024-12-24 15:58:35 +0500

    updating

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 generateCV = () => {
     let name = document.getElementById('name').value;
-    let fatherName = document.getElementById('fatherName').value;
+    let contact = document.getElementById('contact').value;
     let email = document.getElementById('email').value;
     let skills = document.getElementById('skills').value;
     let education = document.getElementById('education').value;
 
     let nameShow = document.getElementById('nameShow');
     nameShow.innerHTML = "Name: "+name;
-    let fatherNameShow = document.getElementById('fatherNameShow');
-    fatherNameShow.innerHTML = "Father Name: "+fatherName;
+    let contactShow = document.getElementById('contactShow');
+    contactShow.innerHTML = "Contact: "+contact;
     let emailShow = document.getElementById('emailShow');
     emailShow.innerHTML = "Email: "+email;
     let skillsShow = document.getElementById('skillsShow');
